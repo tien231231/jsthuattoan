0f96dd4a116fc71325b00dd810aa6764e6d693b4
tien231231 <tien231ub@gmail.com>
2023-04-17 10:49:56 +0700

    done

--- docexcel/test.js
+++ docexcel/test.js
@@ -12,8 +12,8 @@ const data = xlsx.utils.sheet_to_json(worksheet);
 
 const employeeNameRegex = new RegExp(/^NV[a-zA-Z0-9]/);
 
-
-[data[1]].forEach((obj) => {
+const addkey =(opject)=>{
+  [opject].forEach((obj) => {
   let prevValue = null;
   console.log();
   for (let i = 1; i <= 140; i++) {
@@ -25,13 +25,18 @@ const employeeNameRegex = new RegExp(/^NV[a-zA-Z0-9]/);
     }
   }
 });
+}
+addkey(data[1])
+
 let allnv = {};
 
+
+// có bao nhiêu nhân viên thì lặp số lần bấy nhiêu bắt đầu từ opject thứ 4
 for (let x = 4; x < data.length; x++) {
   
-  let summon = 0;
+  let summon = 0;//tạo tổng tiền tháng
   
-  let monray = [];
+  let monray = [];//tạo stack lưu các giá trị tiền fix cứng gc = monray[1]
   
   var nv = {};
   let gc = null;
@@ -51,7 +56,7 @@ for (let x = 4; x < data.length; x++) {
       if (gc === null) {
         gc = data[x][i];
       } else {
-        nv[data[1][i]] = { ...nv[data[1][i]], GC: data[x][i] };
+        nv[data[1][i]] = { ...nv[data[1][i]], GC: (data[x][i]!== undefined ? data[x][i] : 0) };//tìm GC và thên vào 
         // nv[data[1][i]] = { mon: (data[x][i]!==undefined ? data[x][i] : 0)*monray[1] };
       }
     }
@@ -59,38 +64,42 @@ for (let x = 4; x < data.length; x++) {
       if (cn === null) {
         cn = data[x][i];
       } else {
-        nv[data[1][i]] = { ...nv[data[1][i]], CN: data[x][i] };
+        nv[data[1][i]] = { ...nv[data[1][i]], CN: (data[x][i]!== undefined ? data[x][i] : 0)  };
       }
     }
     if (data[3][i] == "GC1") {
       if (gc1 === null) {
         gc1 = data[x][i];
       } else {
-        nv[data[1][i]] = { ...nv[data[1][i]], GC1: data[x][i] };
+        nv[data[1][i]] = { ...nv[data[1][i]], GC1: (data[x][i]!== undefined ? data[x][i] : 0)  };
       }
     }
     if (data[3][i] == "TC") {
       if (tc === null) {
         tc = data[x][i];
       } else {
-        nv[data[1][i]] = { ...nv[data[1][i]], TC: data[x][i] };
+        nv[data[1][i]] = { ...nv[data[1][i]], TC: (data[x][i]!== undefined ? data[x][i] : 0)  };
       }
     }
     if (data[3][i] == "TC1") {
       if (tc1 === null) {
         tc1 = data[x][i];
       } else {
-        nv[data[1][i]] = { ...nv[data[1][i]], TC1: data[x][i] };
+        nv[data[1][i]] = { ...nv[data[1][i]], TC1: (data[x][i]!== undefined ? data[x][i] : 0)  };
       }
     }
   }
   for (let g in nv) {
+    //cộng dần để lấy tổng tiền tháng
     summon +=
       (nv[g]["CN"] !== undefined ? nv[g]["CN"] : 0) * monray[0] +
       (nv[g]["GC"] !== undefined ? nv[g]["GC"] : 0) * monray[1] +
       (nv[g]["TC"] !== undefined ? nv[g]["TC"] : 0) * monray[2] +
       (nv[g]["GC1"] !== undefined ? nv[g]["GC1"] : 0) * monray[3] +
       (nv[g]["TC1"] !== undefined ? nv[g]["TC1"] : 0) * monray[4];
+
+
+    //tổng tiền ngày
     nv[g]["mon"] =
       (nv[g]["CN"] !== undefined ? nv[g]["CN"] : 0) * monray[0] +
       (nv[g]["GC"] !== undefined ? nv[g]["GC"] : 0) * monray[1] +
@@ -98,7 +107,12 @@ for (let x = 4; x < data.length; x++) {
       (nv[g]["GC1"] !== undefined ? nv[g]["GC1"] : 0) * monray[3] +
       (nv[g]["TC1"] !== undefined ? nv[g]["TC1"] : 0) * monray[4];
   }
-  nv["summon"] = summon;
+
+
+  nv["summon"] = summon;//thêm tổng tiền vào tháng
+
+
+  //tạo report so sánh với cột Q trong excel
   if (nv["summon"] < data[x]["__EMPTY_16"]) {
     nv["report"] = "smaller than column Q";
   }
